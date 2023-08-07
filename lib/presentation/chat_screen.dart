4b0d8b735f6245b367ff090fb569a288639087ed
Carlos Buruel Ortiz <carlos.ob.26@gmail.com>
2023-08-07 11:25:55 -0600

    68-ListView-area de mensajes

--- lib/presentation/chat_screen.dart
+++ lib/presentation/chat_screen.dart
@@ -10,11 +10,40 @@ class ChatScreen extends StatelessWidget {
         leading: const Padding(
           padding: EdgeInsets.all(4.0),
           child: CircleAvatar(
-            backgroundImage: NetworkImage('https://styles.redditmedia.com/t5_2gkscp/styles/communityIcon_vcc7ia6xmsab1.jpg?width=256&s=7fc5c5eea41f83ca62e7ef8c3c9ad784bfd95b09'),
+            backgroundImage: NetworkImage(
+                'https://styles.redditmedia.com/t5_2gkscp/styles/communityIcon_vcc7ia6xmsab1.jpg?width=256&s=7fc5c5eea41f83ca62e7ef8c3c9ad784bfd95b09'),
           ),
         ),
         title: const Text('Mi Amor 😻'),
         centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  const _ChatView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return Text('Indice: $index');
+              },
+            )),
+            Text('Mundo'),
+          ],
+        ),
       ),
     );
   }
